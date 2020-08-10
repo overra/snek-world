b1b70eb63a415f2e4296c7457206ae3c61296cb7
Adam Snodgrass <adam@getspruce.com>
2020-08-09 19:07:25 -0500

    prettier formatting

--- snek.ts
+++ snek.ts
@@ -1,12 +1,12 @@
-import { SnekGame } from './lib/SnekGame'
-import './app.css'
+import { SnekGame } from "./lib/SnekGame";
+import "./app.css";
 
 function init() {
-    const snek = document.querySelector('#snek')
-    new SnekGame({
-        container: snek,
-        blockSize: 20
-    })
+  const snek = document.querySelector("#snek");
+  new SnekGame({
+    container: snek,
+    blockSize: 20,
+  });
 }
 
-init()
+init();
